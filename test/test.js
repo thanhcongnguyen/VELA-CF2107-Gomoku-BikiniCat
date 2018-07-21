2661049fa4335a84f619a69735c3e5322b83bfa3
thanhcongnguyen <nguyenthanhcong@unimarmores.com>
2018-07-21 14:31:15 +0700

    add test case

--- test/test.js
+++ test/test.js
@@ -1,8 +1,36 @@
 var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
+describe('GOMOKU', function() {
+  describe('WIN', function() {
+    it('5 quân liên tiếp là thắng', function() {
       assert.equal([1,2,3].indexOf(4), -1);
     });
+    it('4 quân liên tiếp không bị chặn 2 đầu', function() {
+        assert.equal([1,2,3].indexOf(4), -1);
+    });
+    it('4 quân liên tiếp bị chặn một đầu và dãy 3 trắng', function() {
+        assert.equal([1,2,3].indexOf(4), -1);
+      });
+    it('2 dãy 3 trắng', function() {
+        assert.equal([1,2,3].indexOf(4), -1);
+    });
+    it('2 dãy 4 trắng bị chặn 1 đầu', function() {
+        assert.equal([1,2,3].indexOf(4), -1);
+    });
+    it('4 quân liên tiếp bị chặn một đầu và tới lượt đánh', function() {
+        assert.equal([1,2,3].indexOf(4), -1);
+    });
+  });
+  describe('Error', function() {
+    it('mỗi lượt đánh chỉ đuọc đánh vào 1 ô', function() {
+      assert.equal([1,2,3].indexOf(4), -1);
+    });
+    it('không được đánh 2 lần liên tiếp', function() {
+        assert.equal([1,2,3].indexOf(4), -1);
+      });
+    it('ô đã đánh thì không đuợc đánh lại', function() {
+        assert.equal([1,2,3].indexOf(4), -1);
+    });
+    
+    
   });
 });
